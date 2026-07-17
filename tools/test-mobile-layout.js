7b253405bd2ab618f8d8aaa61e6adc197c31579f
Arrowes <395841716@qq.com>
2026-07-17 13:53:06 +0800

    Bust main css cache

--- tools/test-mobile-layout.js
+++ tools/test-mobile-layout.js
@@ -3,6 +3,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 
 const styles = fs.readFileSync(path.join(__dirname, '../blog/source/_data/styles.styl'), 'utf8');
+const head = fs.readFileSync(path.join(__dirname, '../blog/themes/hexo-theme-next/layout/_partials/head/head.swig'), 'utf8');
 const rightPostsOverride = styles.indexOf('@media (max-width: 1420px)');
 const tabletMobileOverride = styles.indexOf('+tablet-mobile()', rightPostsOverride);
 
@@ -13,6 +14,10 @@ const tabletMobileBlock = styles.slice(tabletMobileOverride);
 assert(
   /\.content-wrap\s+width:\s*100%/.test(tabletMobileBlock),
   'tablet/mobile layout should restore content-wrap to full width'
+);
+assert(
+  /main\.css\?v=/.test(head),
+  'main stylesheet should use a version query to avoid stale mobile CSS'
 );
 
 console.log('mobile layout ok');
